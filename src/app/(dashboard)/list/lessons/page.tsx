791f452a70a5ae9c4e872a8556e0affe001d3b9d
Atik2788 <ashanto653.2@gmail.com>
2024-09-08 23:21:07 +0600

    after doing list/lessons page

--- src/app/(dashboard)/list/lessons/page.tsx
+++ src/app/(dashboard)/list/lessons/page.tsx
@@ -1,38 +1,32 @@
 import Pagination from '@/components/Pagination';
 import Table from '@/components/Table';
 import TableSearch from '@/components/TableSearch';
-import { classesData, role, subjectsData } from '@/lib/data';
+import { classesData, lessonsData, role, subjectsData } from '@/lib/data';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
-type Classes = {
+type Lessons = {
     id: number;
-    name: string;
-    capacity: number;
-    grade: number;
-    supervisor: string;
+    subject: string;
+    class: string;
+    teacher: string;
 }
 
 const columns = [
     {
-        header: "Class Names",
-        accessor: 'name',
+        header: "Subject",
+        accessor: 'subject',
 
     },
     {
-        header: "Capacity",
-        accessor: 'capacity',
+        header: "Class",
+        accessor: 'class',
         className: "hidden md:table-cell",
     },
     {
-        header: "Grade",
-        accessor: 'grade',
-        className: "hidden md:table-cell",
-    },
-    {
-        header: "Supervisor",
-        accessor: 'supervisor',
+        header: "Teacher",
+        accessor: 'teacher',
         className: "hidden md:table-cell",
     },
     {
@@ -41,14 +35,13 @@ const columns = [
     },
 ]
 
-const ClassListPage = () => {
+const lessonsListPage = () => {
 
-    const renderRow = (item: Classes) => (
+    const renderRow = (item: Lessons) => (
         <tr key={item.id} className='border-b border-gray-200 even:bg-slate-50 text:sm hover:bg-lamaPurpleLight'>
-            <td className='font-semibold'>{item.name}</td>
-            <td className='hidden md:table-cell'>{item.capacity}</td>
-            <td className='hidden md:table-cell'>{item.grade}</td>
-            <td className='hidden md:table-cell'>{item.supervisor}</td>
+            <td className='font-semibold'>{item.subject}</td>
+            <td className='hidden md:table-cell'>{item.class}</td>
+            <td className='hidden md:table-cell'>{item.teacher}</td>
             <td>
                 <div className='flex items-center gap-2'>
                     <Link href={`/list/teachers/${item.id}`}>
@@ -75,7 +68,7 @@ const ClassListPage = () => {
         <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0'>
             {/* TOP */}
             <div className='flex items-center justify-between'>
-                <h1 className='hidden md:block text-lg font-semibold'>All Classes</h1>
+                <h1 className='hidden md:block text-lg font-semibold'>All Lessons</h1>
                 <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
                     <TableSearch />
                     <div className="flex items-center gap-4 self-end">
@@ -95,7 +88,7 @@ const ClassListPage = () => {
             </div>
 
             {/* LIST */}
-            <Table columns={columns} renderRow={renderRow} data={classesData} />
+            <Table columns={columns} renderRow={renderRow} data={lessonsData} />
 
             {/* PAGINATION */}
             <Pagination />
@@ -104,4 +97,4 @@ const ClassListPage = () => {
     );
 };
 
-export default ClassListPage;
+export default lessonsListPage;
