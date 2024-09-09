5ea516df1ab826417ddcbb23d80235585ef830e4
Atik2788 <ashanto653.2@gmail.com>
2024-09-09 23:08:04 +0600

    after started list/teacher/id single  page

---
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -0,0 +1,26 @@
+
+
+const SingleTeacherPage = () => {
+    return (
+        <div className="flex-1 flex flex-col gap-4 xl:flex-row">
+            {/* LEFT */}
+            <div className="w-full xl:w-2/3">
+                {/* TOP */}
+                <div className=""></div>
+                {/* USER INFO CARD */}
+                <div className=""></div>
+                {/* SMALL CARD */}
+                <div className=""></div>
+                {/* BOTTOM */}
+                <div className="">Schedule</div>
+            </div>
+
+            {/* RIGHT */}
+            <div className="w-full xl:w1/3">
+                Right
+            </div>
+        </div>
+    );
+};
+
+export default SingleTeacherPage;
