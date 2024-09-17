771f8877f0a8b72f65ea8b01f28f75a4953d9d63
Atik2788 <ashanto653.2@gmail.com>
2024-09-17 21:08:46 +0600

    after doing formate Modal to show a modal

--- src/app/(dashboard)/list/students/page.tsx
+++ src/app/(dashboard)/list/students/page.tsx
@@ -81,14 +81,14 @@ const StudentListPage = () => {
             </button>
           </Link>
 
-          <Link href={`/list/teachers/${item.id}`}>
+          {/* <Link href={`/list/teachers/${item.id}`}> */}
             {role === "admin" && (
               // <button className='w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple'>
               //     <Image src="/delete.png" alt='' width={16} height={16} />
               // </button>
               <FormModal table="student" type="delete" id={item.id} />
             )}
-          </Link>
+          {/* </Link> */}
         </div>
       </td>
     </tr>
@@ -109,9 +109,9 @@ const StudentListPage = () => {
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
             {role === "admin" && (
-              /*  <button className='w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow'>
-                                <Image src="/plus.png" alt='' width={14} height={14} />
-                            </button>  */
+              //  <button className='w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow'>
+              //                   <Image src="/plus.png" alt='' width={14} height={14} />
+              //               </button> 
               <FormModal table="student" type="create" />
             )}
           </div>
