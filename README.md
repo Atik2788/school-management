bf7036fe9bce0ffd83564897758ba573678d7ff5
Atik2788 <ashanto653.2@gmail.com>
2024-09-23 21:07:27 +0600

    read me

--- README.md
+++ README.md
@@ -25,10 +25,15 @@ Open [http://localhost:3000](http://localhost:3000) with your browser to see
 #### for react hook form, Zod, Rejosvers
 ###### npm install react-hook-form zod @hookform/resolvers
 
+#### for Prisma
+###### npm i prisma   
+###### npx prisma init
+
 ### Site Links
 ###### https://react-hook-form.com/get-started
 ###### https://zod.dev/
 ###### https://www.npmjs.com/package/@hookform/resolvers#zod
+###### https://www.prisma.io/docs/getting-started/setup-prisma/start-from-scratch/relational-databases-typescript-postgresql
 
 #### for remove any installation
 ###### npm remove rechart
--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -12,12 +12,17 @@ const TeacherForm = dynamic(() => import("./forms/TeacherForm"), {
 const StudentForm = dynamic(() => import("./forms/StudentForm"), {
   loading: () => <h1>Loading</h1>,
 });
+const AnnouncementForm = dynamic(() => import("./forms/AnnouncementForm"), {
+  loading: () => <h1>Loading</h1>,
+});
+
 
 const forms: {
   [key: string]: (tyep: "create" | "update", data?: any) => JSX.Element;
 } = {
   teacher: (type, data) => <TeacherForm type={type} data={data} />,
   student: (type, data) => <StudentForm type={type} data={data} />,
+  // announcementForm: (type, data) => <AnnouncementForm type={type} data={data} />,
 };
 
 const FormModal = ({
