bc46842a8e75ed5c0bb120d774e32384e3a81670
Atik2788 <ashanto653.2@gmail.com>
2024-09-24 09:01:17 +0600

    after doing some in prisma

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -17,26 +17,25 @@ const InputField = ({
   name,
   defaultValue,
   error,
-  inputProps
+  inputProps,
 }: InputFieldProps) => {
   return (
-  <div className="flex flex-col gap-2 w-full md:w-1/4">
-    <label className="text-xs text-gray-400">{label}</label>
-    <input
-      type={type}
-      {...register(name)}
-      className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full"
-      {...inputProps}
-      defaultValue={defaultValue}
-    />
-    {error?.message && (
-      <p className="text-red-500 text-xs font-semibold">
-        {error?.message?.toString()}
-      </p>
-    )}
-  </div>
+    <div className="flex flex-col gap-2 w-full md:w-1/4">
+      <label className="text-xs text-gray-400">{label}</label>
+      <input
+        type={type}
+        {...register(name)}
+        className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full"
+        {...inputProps}
+        defaultValue={defaultValue}
+      />
+      {error?.message && (
+        <p className="text-red-500 text-xs font-semibold">
+          {error?.message?.toString()}
+        </p>
+      )}
+    </div>
   );
 };
-
 
 export default InputField;
